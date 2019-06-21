6e0703398055b9f839d68f4ee693a4b7ac139683
github username <github email address>
2019-06-21 11:14:40 -0400

    fixxed var

--- src/config.js
+++ src/config.js
@@ -1,8 +1,4 @@
-let API_URL; 
 
-if(process.env["NODE_ENV"]==="development"){
-    API_URL = 'http://localhost:3000'
-} else if (process.env["NODE_ENV"]==='production') {
-    API_URL = 'https://issuehandlrapi.herokuapp.com'
-}
+const API_URL = 'https://issuehandlrapi.herokuapp.com'
+
 export default API_URL
